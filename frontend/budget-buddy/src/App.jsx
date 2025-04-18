8baf17d9e5a9879abd84127cd23ad81a735e013f
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-04-18 18:39:35 +0530

    This is the main React component of app.It usually:Holds overall layout or routing

---
+++ frontend/budget-buddy/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () =>  {
+return(
+  <h1 className="text-3xl font-bold underline text bg-purple-900 bg-clip-padding text-red-600">
+  Hello world!
+</h1>
+)
+}
+
+export default App;
