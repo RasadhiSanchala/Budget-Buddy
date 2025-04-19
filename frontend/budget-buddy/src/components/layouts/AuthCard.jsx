9b867da3163770a0bf054fc7cfb37fb580b9ef9b
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-04-19 16:29:46 +0530

    Created for the staring popUp

---
+++ frontend/budget-buddy/src/components/layouts/AuthCard.jsx
@@ -0,0 +1,8 @@
+const AuthCard = ({ children }) => (
+    <div className="bg-white rounded-2xl shadow-lg hover:shadow-2xl transition-shadow duration-300 p-10 w-full max-w-md text-center">
+      {children}
+    </div>
+  );
+  
+  export default AuthCard;
+  
