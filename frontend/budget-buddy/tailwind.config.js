93762c0bfb05ba991d555ffb8f38391d54cc44ca
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-04-19 16:27:42 +0530

    creating a custom animation called "fade" that can be used in Tailwind CSS classes.

--- frontend/budget-buddy/tailwind.config.js
+++ frontend/budget-buddy/tailwind.config.js
@@ -5,8 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fade: 'fadeIn 10s ease-in-out infinite alternate',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0.1' },
+          '100%': { opacity: '0.3' },
+        },
+      },
+    },
   },
   plugins: [],
 }
-
