fd5750c788537d18e00669446372a7be6fc06e33
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-04-19 16:31:05 +0530

    Logo of web site

---
+++ frontend/budget-buddy/src/components/layouts/Logo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from '../../assets/images/logo.png';
+
+const Logo = () => (
+  <img src={logo} alt="Logo" className="w-[200px] mb-20" />
+);
+
+export default Logo;
