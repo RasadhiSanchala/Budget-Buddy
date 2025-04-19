f426372ecd011d31c0f2b2eab041f617973122e3
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-04-19 16:29:19 +0530

    Added all routes that i wished to develop this web site

--- frontend/budget-buddy/src/App.jsx
+++ frontend/budget-buddy/src/App.jsx
@@ -1,11 +1,44 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate 
+} from "react-router-dom";
 
-const App = () =>  {
-return(
-  <h1 className="text-3xl font-bold underline text bg-purple-900 bg-clip-padding text-red-600">
-  Hello world!
-</h1>
-)
-}
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+import Expense from "./pages/Dashboard/Expense";
+import Income from "./pages/Dashboard/Income";
+import Home from "./pages/Dashboard/Home"
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+         <Route path="/Login" exact element={<Login/>} /> 
+         <Route path="/Signup" exact element={<Signup/>} /> 
+         <Route path="/Login" exact element={<Login/>} /> 
+         <Route path="/Expense" exact element={<Expense/>} /> 
+         <Route path="/Home" exact element={<Home/>} /> 
+         <Route path="/Income" exact element={<Income/>} /> 
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
+ const Root = () => {
+  const isAuthenticated = !!localStorage.getItem("token");
+ 
+ return  isAuthenticated?(
+  <Navigate to="/Home"/>
+ ) : (
+  <Navigate to="/Login"/>
+ );
+};
