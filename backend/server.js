99cc70b23f5bf75f1dba142ab144435c4630bb3f
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-04-26 23:10:42 +0530

    Configure server to serve static files from uploads folder

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
 
 const app = express();
 
@@ -17,8 +19,15 @@ app.use(
 
 app.use(express.json());
 
-// Connect to Database
 connectDB();
+
+app.use("/api/v1/auth",authRoutes);
+
+
+//server uploads folder
+app.use("/uploads",express.static(path.join(__dirname,"uploads")));
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
