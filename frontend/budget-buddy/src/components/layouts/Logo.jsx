bdacc60637161163b4ac76ee6320393ced44361f
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-01 16:17:47 +0530

    Resized the logo

--- frontend/budget-buddy/src/components/layouts/Logo.jsx
+++ frontend/budget-buddy/src/components/layouts/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from '../../assets/images/logo.png';
 
 const Logo = () => (
-  <img src={logo} alt="Logo" className="w-[200px] mb-20" />
+  <img src={logo} alt="Logo" className="w-[150px] mb-20" />
 );
 
 export default Logo;
