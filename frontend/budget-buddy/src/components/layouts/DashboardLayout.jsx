517a0ed6fa8ddb7e35e6e492deebcd58233ce782
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-02 19:38:25 +0530

    No changes

--- frontend/budget-buddy/src/components/layouts/DashboardLayout.jsx
+++ frontend/budget-buddy/src/components/layouts/DashboardLayout.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { UserContext } from "../../context/userContext"; 
+import { UserContext } from "../../context/userContext";
 
 import SideMenu from "./SideMenu";
 
@@ -8,10 +8,10 @@ function DashboardLayout({ children, activeMenu }) {
   const { user } = useContext(UserContext);
 
   return (
-      <div className="flex min-h-screen bg-gray-100">
-        <div className="w-80 hidden md:block">
-          <SideMenu activeMenu={activeMenu} />
-        
+    <div className="flex min-h-screen bg-gray-100">
+      <div className="w-80 hidden md:block">
+        <SideMenu activeMenu={activeMenu} />
+
 
         <div className="grow p-5">
           {children}
