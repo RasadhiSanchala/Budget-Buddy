64dd97c6ea802a1079c0edbda8cc5dc8c9c49b7e
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-04 07:27:40 +0530

    Added summaryCard component

--- frontend/budget-buddy/src/pages/Dashboard/Home.jsx
+++ frontend/budget-buddy/src/pages/Dashboard/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import DashboardLayout from '../../components/layouts/DashboardLayout';
+import SummaryCards from '../../components/Dashboard/SummaryCards';
 
 function Home() {
   return (
+    <div className="flex min-h-screen">
+      
     <DashboardLayout activeMenu = "Dashboard">
     <div className='my-5 mx-auto'></div>
     </DashboardLayout>
+
+    <div className="p-12 ">
+        <SummaryCards/>
+      </div>
+
+    </div>
+
+    
   )
 }
 
