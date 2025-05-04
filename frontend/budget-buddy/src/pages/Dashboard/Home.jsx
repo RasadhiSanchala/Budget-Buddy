a31d76086a477386999ce406e2c20a0751e07f53
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-04 16:14:16 +0530

    Changed the layout contnet

--- frontend/budget-buddy/src/pages/Dashboard/Home.jsx
+++ frontend/budget-buddy/src/pages/Dashboard/Home.jsx
@@ -7,26 +7,31 @@ import FinancialOverview from '../../components/Dashboard/FinancialOverview';
 function Home() {
   return (
     <div className="flex min-h-screen">
-      
-    <DashboardLayout activeMenu = "Dashboard">
-    <div className='my-5 mx-auto'></div>
-    </DashboardLayout>
-
-    <div className="p-12 ">
-        <SummaryCards/>
-        <div className="">
-        <RecentTransactions/>
-      </div>
+      {/* Sidebar */}
+      <div className="fixed top-0 left-0 h-screen w-[250px] z-50">
+        <DashboardLayout activeMenu="Dashboard" />
       </div>
 
-    <div>
-      <FinancialOverview/>
-    </div>
+      {/* Main Content */}
+      <div className="flex-1 ml-[250px] p-8 space-y-12">
+        
+        {/* Summary Cards in a Row */}
+        <div className="flex flex-col lg:flex-row gap-6 w-full">
+          <SummaryCards />
+        </div>
 
+        {/* Row: Recent Transactions and Financial Overview */}
+        <div className="flex flex-col lg:flex-row gap-8 w-full">
+          <div className="w-full lg:w-1/2">
+            <RecentTransactions />
+          </div>
+          <div className="w-full lg:w-1/2">
+            <FinancialOverview />
+          </div>
+        </div>
+      </div>
     </div>
-
-    
-  )
+  );
 }
 
 export default Home;
