7001354d9cd0ffeaa40e4977db9203ee55a76ad7
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-04 16:55:12 +0530

    Imported the footer

--- frontend/budget-buddy/src/pages/Dashboard/Home.jsx
+++ frontend/budget-buddy/src/pages/Dashboard/Home.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import DashboardLayout from '../../components/layouts/DashboardLayout';
 import SummaryCards from '../../components/Dashboard/SummaryCards';
 import RecentTransactions from '../../components/Dashboard/RecentTransactions';
 import FinancialOverview from '../../components/Dashboard/FinancialOverview';
+import Footer from '../../components/layouts/Footer';
 
 function Home() {
+  // Scroll to top on load
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="flex min-h-screen">
       {/* Sidebar */}
-      <div className="fixed top-0 left-0 h-screen w-[250px] z-50">
+      <div className="fixed top-0 left-0 h-screen w-[280px] z-50">
         <DashboardLayout activeMenu="Dashboard" />
       </div>
 
@@ -21,13 +27,18 @@ function Home() {
         </div>
 
         {/* Row: Recent Transactions and Financial Overview */}
-        <div className="flex flex-col lg:flex-row gap-8 w-full">
+        <div className="flex flex-col lg:flex-row gap-16 w-full ml-10">
           <div className="w-full lg:w-1/2">
             <RecentTransactions />
           </div>
           <div className="w-full lg:w-1/2">
             <FinancialOverview />
           </div>
+        </div>
+
+        {/* Footer */}
+        <div>
+          <Footer />
         </div>
       </div>
     </div>
