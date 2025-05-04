9548ffeca3eeb0438965ddf7d3e930da8a92b089
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-04 17:14:02 +0530

    Imported the footer section

--- frontend/budget-buddy/src/pages/Dashboard/Home.jsx
+++ frontend/budget-buddy/src/pages/Dashboard/Home.jsx
@@ -6,27 +6,27 @@ import FinancialOverview from '../../components/Dashboard/FinancialOverview';
 import Footer from '../../components/layouts/Footer';
 
 function Home() {
-  // Scroll to top on load
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
     <div className="flex min-h-screen">
-      {/* Sidebar */}
+     
       <div className="fixed top-0 left-0 h-screen w-[280px] z-50">
         <DashboardLayout activeMenu="Dashboard" />
       </div>
 
-      {/* Main Content */}
+   
       <div className="flex-1 ml-[250px] p-8 space-y-12">
         
-        {/* Summary Cards in a Row */}
+       
         <div className="flex flex-col lg:flex-row gap-6 w-full">
           <SummaryCards />
         </div>
 
-        {/* Row: Recent Transactions and Financial Overview */}
+ 
         <div className="flex flex-col lg:flex-row gap-16 w-full ml-10">
           <div className="w-full lg:w-1/2">
             <RecentTransactions />
@@ -36,7 +36,7 @@ function Home() {
           </div>
         </div>
 
-        {/* Footer */}
+       
         <div>
           <Footer />
         </div>
